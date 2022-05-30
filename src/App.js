587dd0731f0eb6e3c62f20f392afc26e92647085
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-05-30 16:47:30 +0200

    préparation du caroussel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import Home from "./components/pages/Home";
+import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom"
 
 function App() {

a3fb50e48ee2e81511771d720e1da660d4f41525
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-05-31 17:35:25 +0200

    UI Netflix

--- src/Requests.js
+++ src/Requests.js
@@ -5,7 +5,8 @@ const requests = {
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
     requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
-    requestRecommendations: `https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key=${key}&language=en-US&page=1`,
+    requestNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
+    
 }
 
 export default requests
